efb23a5b4d8079eb81718ff71896875e8439975e
ej9x <strcmp9@gmail.com>
2019-05-08 20:06:42 +0300

    build: add babel to ts build

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  presets: [
+    '@babel/env',
+    '@babel/typescript'
+  ],
+
+  plugins:
+    [
+      '@babel/proposal-class-properties',
+      '@babel/proposal-object-rest-spread',
+      ['babel-plugin-inline-import', {
+      extensions: [
+        '.ejs',
+      ],
+    }],
+  ],
+};
